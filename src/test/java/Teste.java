32409d6380a04ba65614c305be7106dd647db764
Lucas Ramon <lucasoliveirainor3105@gmail.com>
2021-03-14 20:05:58 -0300

    comitando a calculadora porem com erros

---
+++ src/test/java/Teste.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lucas
+ */
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double c=(8);
+        double b= 4;
+        
+        System.out.println(c*b);
+    }
+    
+}
